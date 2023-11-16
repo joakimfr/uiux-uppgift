d5566c26e9fda9e901e459eaafceebd3e9eae751
joakimfr <fritzjoakim@hotmail.com>
2023-11-16 09:15:44 +0100

    next js and pages setup

---
+++ portfolio/app/contact/page.jsx
@@ -0,0 +1,9 @@
+
+export default function contact () {
+
+  return (
+    <main>
+      <h2>Contact</h2>
+    </main>
+  )
+}
---
+++ portfolio/app/cv/page.jsx
@@ -0,0 +1,10 @@
+
+
+export default function cv () {
+
+  return (
+    <main>
+      <h2>CV</h2>
+    </main>
+  )
+}

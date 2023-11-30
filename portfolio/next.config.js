7570c22e2f07543122ea80ffcc1d22164b087ad6
joakimfr <fritzjoakim@hotmail.com>
2023-11-30 14:04:39 +0100

    qqqq

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,12 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      // Lägg till dina andra vägar här
-    };
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
